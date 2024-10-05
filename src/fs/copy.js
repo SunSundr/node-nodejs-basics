1703d8f4bf9e03f240feb79d7a300a0ea694fe78
SunSundr <art.dizigner@gmail.com>
2024-10-05 17:56:08 +0400

    feat: add copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,20 @@
+import fs from 'node:fs/promises';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { output } from './../output.js';
+
+const srcDir = path.join(dirname(fileURLToPath(import.meta.url)), 'files');
+const destDir = `${srcDir}_copy`;
+const msgPrefix = 'FS operation';
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        await fs.mkdir(destDir);
+        await fs.cp(srcDir, destDir, { recursive: true, force: false, errorOnExist: true });
+        console.log(output('green', `${msgPrefix} success:`),  `folder '${destDir}' has been copied\n`);
+    } catch (err) {
+        console.error(output('red', `${msgPrefix} failed:`),  err.message, '\n');
+    }
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -5,11 +5,12 @@ import { output } from './../output.js';
 
 const filePath = path.join(dirname(fileURLToPath(import.meta.url)), 'files', 'fresh.txt');
 const fileContent = 'I am fresh and young';
+const msgPrefix = 'FS operation';
 
 const create = async () => {
     try {
         await fs.writeFile(filePath, fileContent, { flag: 'wx+' });
-        console.log(output('green', `FS operation success:`),  `file "${filePath}" has been created\n`);
+        console.log(output('green', `${msgPrefix} success:`),  `file "${filePath}" has been created\n`);
 
     } catch (err) {
         console.error(output('red', `FS operation failed:`),  err.code === 'EEXIST' ? 'file already exists' : err.message, '\n');
