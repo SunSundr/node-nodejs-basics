8eb5280bc576a7e0ab8820bbe0b9dbff4c0506a4
SunSundr <art.dizigner@gmail.com>
2024-10-05 16:12:20 +0400

    feat: add create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import fs from 'node:fs/promises';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const filePath = path.join(dirname(fileURLToPath(import.meta.url)), 'files', 'fresh.txt');
+const fileContent = 'I am fresh and young';
+const msgPrefix = 'FS operation';
+
 const create = async () => {
-    // Write your code here 
+    try {
+        await fs.writeFile(filePath, fileContent, { flag: 'wx+' });
+        console.log(`${msgPrefix} success: file "${filePath}" has been created`);
+    } catch (err) {
+        console.error(`${msgPrefix} failed: `,  err.code === 'EEXIST' ? 'file already exists' : err.message);
+    }
 };
 
 await create();
