0c7b0b9e29c42d65170de8e7471cf8b814de6e1a
SunSundr <art.dizigner@gmail.com>
2024-10-05 20:50:37 +0400

    feat: add parseArgs function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,21 @@
+import { output } from './../output.js';
+
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    const argsResult = [];
+
+    for (let i = 0; i < args.length; i += 2) {
+        const propName = args[i].replace('--', '');
+        const value = args[i + 1];
+        argsResult.push(`${propName} is ${value}`);
+    }
+
+    if (argsResult.length) {
+        console.log(output('green', `[Done] Command line arguments:`));
+        console.log(output('cyan', argsResult.join('\n')), '\n');
+    } else {
+        console.log(output('red', `[Error] There are no command line arguments`));
+    }
 };
 
 parseArgs();
