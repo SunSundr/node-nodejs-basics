b2b163bbec45fcb9701dd00e84c51d9ebc17e1fe
SunSundr <art.dizigner@gmail.com>
2024-10-05 18:46:30 +0400

    feat: add remove function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import fs from 'node:fs/promises';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { output } from './../output.js';
+
+const filePath = path.join(dirname(fileURLToPath(import.meta.url)), 'files', 'fileToRemove.txt');
+const msgPrefix = 'FS operation';
+
 const remove = async () => {
-    // Write your code here 
+    try {
+        await fs.unlink(filePath);
+        console.log(output('green', `${msgPrefix} success:`),  `file '${filePath}' has been removed\n`);
+    } catch (err) {
+        console.error(output('red', `FS operation failed:`),  err.message, '\n');
+    }
 };
 
 await remove();
